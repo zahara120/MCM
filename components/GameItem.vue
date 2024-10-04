e7500214b89fb5f13afc88507eaa9be4545cd4f7
zahara120 <zaharaa120@gmail.com>
2024-10-04 20:59:44 +0700

    feat: add game item

---
+++ components/GameItem.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex items-center gap-3">
+        <NuxtImg class="rounded-lg w-[50px] h-[50px] object-cover" src="/img/header1.jpg" />
+        <div class="flex flex-col">
+            <h1 class="text-sm">game name</h1>
+            <span class="text-gray-400 text-xs">description</span>
+        </div>
+    </div>
+</template>
