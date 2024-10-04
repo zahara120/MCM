31f53ded7180b8734a9d6011414a52509f09ff9d
zahara120 <zaharaa120@gmail.com>
2024-10-04 17:58:05 +0700

    style: navbar & footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="flex border-t w-full h-[48px]">
+    <footer class="flex border-t border-slate-800 w-full h-[48px]">
         <div class="container mx-auto max-w-screen-lg p-5 flex flex-1 items-center">
             <h1>2024</h1>
         </div>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,12 @@
 <template>
-    <header class="flex border-b w-full h-[68px]">
-        <div class="container mx-auto max-w-screen-lg p-5 flex flex-1 items-center">
-            <h1>navbar</h1>
+    <header class="flex border-b border-slate-800 w-full h-[68px]">
+        <div class="container mx-auto max-w-screen-lg p-5 flex gap-24">
+            <NuxtLink to="/">Logo</NuxtLink>
+            <div class="flex items-center gap-6 text-sm capitalize">
+                <NuxtLink to="/">all games</NuxtLink>
+                <NuxtLink to="/">winner of the day</NuxtLink>
+                <NuxtLink to="/">recent bids</NuxtLink>
+            </div>
         </div>
     </header>
 </template>
