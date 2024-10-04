651a04a3adfaec93147efcceaff62f32a3dcd1c1
zahara120 <zaharaa120@gmail.com>
2024-10-04 20:43:43 +0700

    style: navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="flex border-b border-slate-800 w-full h-[68px]">
-        <div class="container mx-auto max-w-screen-lg p-5 flex gap-24">
+    <header class="flex justify-between border-b border-slate-800 w-full h-[68px] p-5">
+        <div class="flex items-center gap-6">
             <NuxtLink to="/">Logo</NuxtLink>
             <div class="flex items-center gap-6 text-sm capitalize">
                 <NuxtLink to="/">all games</NuxtLink>
@@ -8,5 +8,22 @@
                 <NuxtLink to="/">recent bids</NuxtLink>
             </div>
         </div>
+        <nav class="flex items-center gap-4">
+            <button
+                class="flex items-center gap-2 rounded-lg hover:bg-green-800/70 focus:bg-green-800/100 bg-green-800/50 px-4 py-2 uppercase text-xs">
+                <Icon name="unjs:automd" style="color: black" /> feyyora
+                utilities
+            </button>
+            <button
+                class="flex items-center gap-2 rounded-lg hover:bg-indigo-800/70 focus:bg-indigo-800/100 bg-indigo-800/50 px-4 py-2 uppercase text-xs">
+                <Icon name="cryptocurrency-color:generic" style="color: black" /> win
+                bonuses
+            </button>
+            <button
+                class="flex items-center gap-2 rounded-lg hover:bg-blue-800/70 focus:bg-blue-800/100 bg-blue-800/50 px-4 py-2 uppercase text-xs">
+                <Icon name="unjs:confbox" style="color: black" /> feyyora
+                utilities
+            </button>
+        </nav>
     </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,74 @@
 <script setup lang="ts">
+
+import GameCatalog from '~/components/GameCatalog.vue';
 definePageMeta({
   layout: "page",
 })
 </script>
 
 <template>
-  <div class="flex-1 flex flex-col justify-center items-center">
-    <h1 class="text-3xl">Home page</h1>
-    <p>
-      ini nanti nya bakal pake bento
-    </p>
+  <div class="grid grid-cols-10 gap-5 w-full p-5">
+    <div class="col-span-8 flex flex-col gap-5">
+      <NuxtImg class="rounded-xl w-full max-h-[380px] aspect-[16/9] object-cover" src="/img/header1.jpg" />
+
+      <div class="flex flex-col gap-8">
+        <section class="flex items-center justify-between">
+          <!-- list of tabs -->
+          <ul class="flex items-center gap-8">
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">all game</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">new</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">popular</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">slots</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">table game</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">live casino</button>
+            </li>
+            <li>
+              <button class="capitalize text-xs hover:border-b cursor-pointer">jackpots</button>
+            </li>
+          </ul>
+
+          <!-- search -->
+          <form action="">
+            <input type="text" placeholder="search game"
+              class="min-w-[200px] text-xs rounded-lg px-4 py-3 m-0 bg-secondary">
+          </form>
+        </section>
+
+        <!-- list of games -->
+        <section class="grid grid-cols-5 gap-3">
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+        </section>
+
+        <!-- list of games -->
+        <section class="grid grid-cols-3 gap-3">
+          <GameCatalog />
+          <GameCatalog />
+          <GameCatalog />
+        </section>
+      </div>
+    </div>
+    <aside class="sticky top-0 self-start rounded-xl col-span-2 bg-secondary p-5 w-full h-screen">
+      <h1 class="w-full">Ini seharusnya isi chat</h1>
+    </aside>
   </div>
 </template>
