3419fb4b1bd2a9611c7fcd65261826a82d68055a
zahara120 <zaharaa120@gmail.com>
2024-10-05 10:46:05 +0700

    style: responsive footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="flex border-t border-slate-800 w-full h-[48px] bg-primary text-white">
         <div class="container mx-auto max-w-screen-lg p-5 flex flex-1 items-center justify-between">
             <h1 class="text-sm">&copy; 2024 Zahara Amalia. All rights reserved.</h1>
-            <nav class="flex space-x-4">
+            <nav class="hidden md:flex space-x-4">
                 <NuxtLink to="/" class="text-sm hover:underline">About</NuxtLink>
                 <NuxtLink to="/" class="text-sm hover:underline">Privacy Policy</NuxtLink>
                 <NuxtLink to="/" class="text-sm hover:underline">Terms of Service</NuxtLink>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
     <header class="flex justify-between border-b border-slate-800 w-full h-[68px] p-5 relative">
+        <!-- <div v-if="menu" class="bg-slate-800/80 w-full h-screen absolute top-0 left-0 z-10"></div> -->
+        <transition enter-active-class="transition-opacity duration-500" enter-from-class="opacity-0"
+            enter-to-class="opacity-100" leave-active-class="transition-opacity duration-500"
+            leave-from-class="opacity-100" leave-to-class="opacity-0">
+            <div v-if="menu" class="bg-slate-800/80 w-full h-screen absolute top-0 left-0 z-10"></div>
+        </transition>
         <div class="flex items-center gap-6 justify-between md:justify-normal w-full">
             <NuxtLink to="/">Logo</NuxtLink>
             <div class="hidden md:flex items-center gap-6 text-sm capitalize">
@@ -29,17 +35,21 @@
         </nav>
 
         <!-- mobile nav -->
-        <div v-if="menu" class="block md:hidden h-screen w-1/2 bg-white fixed top-0 right-0 z-50 p-5 text-black">
-            <button class="focus:text-red-500 absolute top-2 right-5" @click="menu = !menu">X</button>
-            <div class="flex flex-col gap-5">
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
-                <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+        <transition enter-active-class="transition-opacity duration-500" enter-from-class="opacity-0"
+            enter-to-class="opacity-100" leave-active-class="transition-opacity duration-500"
+            leave-from-class="opacity-100" leave-to-class="opacity-0">
+            <div v-if="menu" class="block md:hidden h-screen w-1/2 bg-white fixed top-0 right-0 z-50 p-5 text-black">
+                <button class="focus:text-red-500 absolute top-2 right-5" @click="menu = !menu">X</button>
+                <div class="flex flex-col gap-5 mt-3">
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                    <NuxtLink class="focus:bg-secondary focus:text-white p-2 rounded-xl" to="/">Home</NuxtLink>
+                </div>
             </div>
-        </div>
+        </transition>
     </header>
 </template>
 
