7aeface1ea0f35fb5e360d22bb8edb142ab7a5c1
zahara120 <zaharaa120@gmail.com>
2024-10-08 10:46:20 +0700

    feat: wiring homepage

--- components/GameItem.vue
+++ components/GameItem.vue
@@ -1,17 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface GameData {
+  id: number;
+  title: string;
+  genre: string;
+  thumbnail: string;
+}
+defineProps<{
+  data: GameData;
+}>();
+</script>
 
 <template>
   <NuxtLink
-    to="/detail/1"
+    :to="`/detail/${data?.id}`"
     class="flex items-center gap-3 hover:bg-primary/50 cursor-pointer rounded-xl px-1 py-2"
   >
     <NuxtImg
       class="rounded-lg w-[50px] h-[50px] object-cover"
-      src="/img/header1.jpg"
+      :src="data?.thumbnail"
     />
     <div class="flex flex-col">
-      <h1 class="text-sm">game name</h1>
-      <span class="text-gray-400 text-xs">description</span>
+      <h1 class="text-sm">{{ data.title }}</h1>
+      <span class="text-gray-400 text-xs">{{ data.genre }}</span>
     </div>
   </NuxtLink>
 </template>
